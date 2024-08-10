6edc55b1286c7f20aafd2f10a06d754d37d614f2
amegh-ts <80452444+amegh-ts@users.noreply.github.com>
2024-08-10 20:54:50 +0530

    change

--- src/requestMethods.ts
+++ src/requestMethods.ts
@@ -1,19 +1,19 @@
-import axios from "axios"
+import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/";
-const BASE_URL = "https://etheredge-server.onrender.com/";
+const BASE_URL = "http://localhost:5000/";
+// const BASE_URL = "https://etheredge-server.onrender.com/";
 
-const storedData = localStorage.getItem('persist:alpha96');
+const storedData = localStorage.getItem("persist:alpha96");
 const user = storedData ? JSON.parse(JSON.parse(storedData).user) : null;
 
 const Token = user?.userInfo?.[0]?.accessToken;
 // console.log(Token);
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
-    headers: { token: `Bearer ${Token}` }
-}) 
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${Token}` },
+});
---
+++ src/root/root.tsx
@@ -0,0 +1,26 @@
+import { Outlet } from "react-router-dom";
+
+
+
+export default function Root() {
+
+    return (
+        <div className="RootContainer">
+            <div className="Sidebar">
+                {/* <Sidebar /> */}
+            </div>
+            <div className="MainBody">
+                <div className="header">
+                    {/* <Navbar /> */}
+                </div>
+
+                <div className="content">
+                    <Outlet />
+                </div>
+                <div className="footer">
+                    {/* <Footer /> */}
+                </div>
+            </div>
+        </div>
+    );
+}

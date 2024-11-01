e68ce997252daf36e50d6180dc9fb4b8321d5a0e
amegh-ts <amegh2002@gmail.com>
2024-11-01 20:08:41 +0530

    rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.scss";
-import Root from "./root/root";
-import Dashboard from "./pages/dashboard/dashboard";
-import Profile from "./pages/profile/profile";
-import EditProfile from "./pages/profile/components/editProfile";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      element: <Root />,
-      children: [
-        {
-          path: "/",
-          element: <Dashboard />,
-        },
-        {
-          path: "/profile",
-          element: <Profile />,
-        },
-        {
-          path: "/profile/editProfile",
-          element: <EditProfile />,
-        },
-      ],
-    },
-  ]);
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <RouterProvider router={router} />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import Verify from "./verify";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Verify />
-          </PersistGate>
-        </Provider>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </ChakraProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)

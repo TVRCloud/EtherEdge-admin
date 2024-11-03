9bf232c60954b72b351a9e96f1698c85d0d83515
amegh-ts <amegh2002@gmail.com>
2024-11-03 15:50:14 +0530

    remove: theme toggle btn

--- src/pages/Dashboard/page.tsx
+++ src/pages/Dashboard/page.tsx
@@ -1,17 +1,7 @@
 const Dashboard = () => {
-  const toggleDarkMode = () => {
-    document.documentElement.classList.toggle("dark");
-  };
   return (
     <div>
       <h1>Dashboard</h1>
-
-      <button
-        onClick={toggleDarkMode}
-        className="p-2 bg-primary-color dark:bg-dark-primary-color text-white"
-      >
-        Toggle Dark Mode
-      </button>
     </div>
   );
 };

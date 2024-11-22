8721bc852189652dd74926844d787f8a9e3fe251
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-11-23 04:04:58 +0530

    Update EditProfileImage.tsx

--- src/components/profile/EditProfileImage.tsx
+++ src/components/profile/EditProfileImage.tsx
@@ -73,7 +73,7 @@ const EditProfileImage = () => {
         <form>
           <div className="w-full h-[100px] sm:h-[120px] md:h-[140px] lg:h-[160px] bg-[url('/assets/profile/profileBanner.webp')] bg-cover bg-no-repeat rounded-[16px] relative">
             <div
-              className="bg-main-bg dark:bg-dark-main-bg p-2 h-[140px] w-[140px] rounded-full overflow-hidden absolute bottom-[-70px] left-1/2 transform -translate-x-1/2 cursor-pointer relative"
+              className="bg-main-bg dark:bg-dark-main-bg p-2 h-[140px] w-[140px] rounded-full overflow-hidden absolute bottom-[-70px] left-1/2 transform -translate-x-1/2 cursor-pointer"
               onClick={handleImageClick}
             >
               <img

37e6886b4b3728de1bf322b591e5477ae6ad27d1
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-11-23 01:56:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+      "@root": path.resolve(__dirname, "src/root"),
+      "@schema": path.resolve(__dirname, "src/schema"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
 });

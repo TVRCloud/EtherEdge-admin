01af501685e25668faec552eae1bd20fa5cd422b
Amegh <80452444+amegh-ts@users.noreply.github.com>
2024-11-23 15:38:29 +0530

    Update page.tsx

--- src/pages/profile/editProfile/page.tsx
+++ src/pages/profile/editProfile/page.tsx
@@ -1,11 +1,20 @@
 import EditProfileImage from "@components/profile/EditProfileImage";
 import EditProfileBasicDetails from "@components/profile/EditProfileBasicDetails";
+import useUserDetails from "@utils/useUserDetails";
 
 const EditProfile = () => {
+  const { data, isLoading } = useUserDetails();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="grid grid-cols-2 gap-4">
-      <EditProfileImage />
-      <EditProfileBasicDetails />
+      <div className="flex flex-col gap-3">
+        <EditProfileImage data={data} />
+        <EditProfileBasicDetails data={data} />
+      </div>
     </div>
   );
 };

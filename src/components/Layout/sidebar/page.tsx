0ff7dbd38482493c86676ffa2282e63cc9a412f0
Amegh <amegh2002@gmail.com>
2025-02-15 11:31:23 +0530

    Fix: button issue

--- src/components/Layout/sidebar/page.tsx
+++ src/components/Layout/sidebar/page.tsx
@@ -1,7 +1,6 @@
 import { GiAbstract111 } from "react-icons/gi";
 
 import NestedSidebar from "./components/Navigations";
-import { Button } from "../../ui/button";
 
 const SideBar = () => {
   return (
@@ -18,14 +17,6 @@ const SideBar = () => {
 
         {/* footer */}
         <div className="flex justify-center flex-col gap-9 mb-8">
-          {/* <div className="w-full bg-gradient-to-b from-[#7450fe] to-[#3513dc] text-white flex flex-col gap-1 justify-center items-center p-6 rounded-[24px]">
-            <h3 className="text-2xl font-bold">Title</h3>
-            <h4 className="text-justify">Subtitle</h4>
-            <Button className="bg-[#e6faf5] text-[#01b574] h-min py-1 px-3 rounded-full">
-              dgsdg
-            </Button>
-          </div> */}
-
           {/* avatar */}
           <div className="flex gap-3 items-center ">
             <div className="h-[45px] w-[45px] rounded-full overflow-hidden">
